f7f159ee5eece008086220ebf13903f7b8e75b95
KotlinNLP Authors <github@kotlinnlp.com>
2017-08-10 15:34:33 +0200

    Remove package declaration

--- examples/main.kt
+++ examples/main.kt
@@ -5,8 +5,6 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  * ------------------------------------------------------------------*/
 
-package examples
-
 import com.kotlinnlp.conllio.CoNLLReader
 import java.io.File
 
